b092bca2098adc098795189f637aab8a95a0c685
Richard Marscher <rmarscher@localytics.com>
2018-06-06 22:15:10 -0400

    Some improvements to the scripts

--- jvmFlameGraphDump.sh
+++ jvmFlameGraphDump.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
 JAVAUSER=$(ps -p $(pgrep java) -o %u | tail -n 1)
 PID=$(pgrep java)
 UNIQID=$(date +%s)
+FILENAME="/tmp/flame-graph-stacks-$UNIQID.jstk"
 
-sudo -u $JAVAUSER jstack $PID > threaddump-$UNIQID.log
+echo "Attempting to build a flame graph dump for $JAVAUSER with pid $PID and writing to $FILENAME"
 
 for i in {1..100}; do
-  sudo -u $JAVAUSER jstack $PID >> flame-graph-stacks-$UNIQID.jstk
+  sudo -u $JAVAUSER jstack $PID >> $FILENAME
+  printf "."
   sleep 0.25
 done
